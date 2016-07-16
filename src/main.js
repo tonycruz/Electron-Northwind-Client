9cf6a9ce278393d0b7a41b472db0cb55b3fdbb5b
Antonio Lopes Da Cruz <tonycruz@outlook.com>
2016-07-16 20:44:54 +0100

    Electron NW Client

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//import {Aurelia} from 'aurelia-framework';
+import {ToastrConfig} from "./modules/toastrConfig";
+export function configure(aurelia) {
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging()
+        .feature("modules")
+        .plugin('aurelia-validation');
+    aurelia.start().then(a => a.setRoot('app'));
+   ToastrConfig.setToastrConfigure();
+}
